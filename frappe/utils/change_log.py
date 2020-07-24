b86a0d14fec4cea514fb96461b7ee630ad4978a5
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-24 06:15:13 +0300

    Get away from master/slave vocabulary

--- frappe/utils/change_log.py
+++ frappe/utils/change_log.py
@@ -103,7 +103,7 @@ def get_versions():
 			"branch": get_app_branch(app)
 		}
 
-		if versions[app]['branch'] != 'master':
+		if versions[app]['branch'] != 'main':
 			branch_version = app_hooks.get('{0}_version'.format(versions[app]['branch']))
 			if branch_version:
 				versions[app]['branch_version'] = branch_version[0] + ' ({0})'.format(get_app_last_commit_ref(app))
